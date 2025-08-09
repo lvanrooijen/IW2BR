d86fa61500eb24f586953b1dcfc25f366859a8c9
Laila <175353500+lvanrooijen@users.noreply.github.com>
2025-08-09 22:50:15 +0200

    chore: Validate note membership in note collection

--- src/main/java/com/bella/IW2BR/domain/notecollection/note/NoteService.java
+++ src/main/java/com/bella/IW2BR/domain/notecollection/note/NoteService.java
@@ -2,12 +2,12 @@ package com.bella.IW2BR.domain.notecollection.note;
 
 import com.bella.IW2BR.domain.environment.util.EnvironmentHelperMethods;
 import com.bella.IW2BR.domain.notecollection.collection.NoteCollection;
-import com.bella.IW2BR.domain.notecollection.collection.NoteCollectionRepository;
 import com.bella.IW2BR.domain.notecollection.note.dto.GetNote;
 import com.bella.IW2BR.domain.notecollection.note.dto.NoteMapper;
 import com.bella.IW2BR.domain.notecollection.note.dto.PatchNote;
 import com.bella.IW2BR.domain.notecollection.note.dto.PostNote;
 import com.bella.IW2BR.domain.tag.Tag;
+import com.bella.IW2BR.exceptions.environment.NoteCollectionMismatchException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,20 +17,18 @@ import org.springframework.stereotype.Service;
 public class NoteService {
   private final NoteMapper noteMapper;
   private final NoteRepository noteRepository;
-  private final NoteCollectionRepository noteCollectionRepository;
-  private final EnvironmentHelperMethods environmentHelperMethods;
+  private final EnvironmentHelperMethods helperMethods;
 
   public GetNote create(Long environmentId, Long noteCollectionId, PostNote body) {
-    environmentHelperMethods.throwIfNotOwnerOrAdmin(environmentId);
+    helperMethods.throwIfNotOwnerOrAdmin(environmentId);
 
-    NoteCollection noteCollection =
-        environmentHelperMethods.getNoteCollectionOrThrow(noteCollectionId);
+    NoteCollection noteCollection = helperMethods.getNoteCollectionOrThrow(noteCollectionId);
 
-    environmentHelperMethods.throwIfNotInEnvironment(noteCollection, environmentId);
+    helperMethods.throwIfNotInEnvironment(noteCollection, environmentId);
 
     Note note;
     if (body.tagId() != null) {
-      Tag tag = environmentHelperMethods.getTagOrThrow(body.tagId());
+      Tag tag = helperMethods.getTagOrThrow(body.tagId());
       note = noteMapper.fromPost(body, noteCollection, tag);
     } else {
       note = noteMapper.fromPost(body, noteCollection);
@@ -42,31 +40,33 @@ public class NoteService {
   }
 
   public GetNote getById(Long environmentId, Long noteCollectionId, Long id) {
-    // TODO is note member van note collection?
-    environmentHelperMethods.throwIfNotOwnerOrAdmin(environmentId);
-    NoteCollection noteCollection =
-        environmentHelperMethods.getNoteCollectionOrThrow(noteCollectionId);
-    environmentHelperMethods.throwIfNotInEnvironment(noteCollection, environmentId);
-    Note note = environmentHelperMethods.getNoteOrThrow(id);
+    helperMethods.throwIfNotOwnerOrAdmin(environmentId);
+
+    NoteCollection noteCollection = helperMethods.getNoteCollectionOrThrow(noteCollectionId);
+    helperMethods.throwIfNotInEnvironment(noteCollection, environmentId);
+
+    Note note = helperMethods.getNoteOrThrow(id);
+    throwIfNoteCollectionIdMismatch(noteCollectionId, note);
 
     return noteMapper.toGet(note);
   }
 
   public List<GetNote> getAll(Long environmentId, Long noteCollectionId) {
-    environmentHelperMethods.throwIfNotOwnerOrAdmin(environmentId);
-    NoteCollection noteCollection =
-        environmentHelperMethods.getNoteCollectionOrThrow(noteCollectionId);
+    helperMethods.throwIfNotOwnerOrAdmin(environmentId);
+    NoteCollection noteCollection = helperMethods.getNoteCollectionOrThrow(noteCollectionId);
     List<Note> notes = noteRepository.findByNotecollection(noteCollection);
 
     return notes.stream().map(noteMapper::toGet).toList();
   }
 
   public GetNote update(Long environmentId, Long noteCollectionId, Long id, PatchNote patch) {
-    // TODO is note member van note collection?
-    environmentHelperMethods.throwIfNotOwnerOrAdmin(environmentId);
-    Note note = environmentHelperMethods.getNoteOrThrow(id);
+    helperMethods.throwIfNotOwnerOrAdmin(environmentId);
+
+    Note note = helperMethods.getNoteOrThrow(id);
+    throwIfNoteCollectionIdMismatch(noteCollectionId, note);
+
     if (patch.tagId() != null) {
-      Tag tag = environmentHelperMethods.getTagOrThrow(patch.tagId());
+      Tag tag = helperMethods.getTagOrThrow(patch.tagId());
       noteMapper.updateFields(note, patch, tag);
     } else {
       noteMapper.updateFields(note, patch);
@@ -77,12 +77,30 @@ public class NoteService {
   }
 
   public void delete(Long environmentId, Long noteCollectionId, Long id) {
-    // TODO is note member van note collection?
-    environmentHelperMethods.throwIfNotOwnerOrAdmin(environmentId);
-    NoteCollection noteCollection =
-        environmentHelperMethods.getNoteCollectionOrThrow(noteCollectionId);
-    environmentHelperMethods.throwIfNotInEnvironment(noteCollection, environmentId);
-    environmentHelperMethods.getNoteOrThrow(id);
+    helperMethods.throwIfNotOwnerOrAdmin(environmentId);
+
+    NoteCollection noteCollection = helperMethods.getNoteCollectionOrThrow(noteCollectionId);
+    helperMethods.throwIfNotInEnvironment(noteCollection, environmentId);
+
+    Note note = helperMethods.getNoteOrThrow(id);
+    throwIfNoteCollectionIdMismatch(noteCollectionId, note);
+
     noteRepository.deleteById(id);
+  }
+
+  /**
+   * Verifies if the noteCollectionId in the path variable is the same as the note-collection
+   * connected to the note
+   *
+   * @param noteCollectionId ID of flashcard provided through the path variable
+   * @param note {@link Note}
+   * @throws NoteCollectionMismatchException when the ID's aren't the same
+   */
+  private void throwIfNoteCollectionIdMismatch(Long noteCollectionId, Note note) {
+    helperMethods.throwIfIdMismatch(
+        noteCollectionId,
+        note.getNotecollection().getId(),
+        new NoteCollectionMismatchException(
+            "Note is not a member of the Note-Collection specified in the path variable"));
   }
 }
